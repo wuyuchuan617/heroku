5550b21868b52e6b0182fdaaca21e032162be9f5
Kimbra Wu <kimbra.wu@gmail.com>
2022-04-07 16:08:00 +0800

    test change to deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to relodad.
+          Edit <code>src/App.js</code> and save to relodddddad.
         </p>
         <a
           className="App-link"
